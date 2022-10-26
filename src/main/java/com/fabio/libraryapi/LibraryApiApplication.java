09e0b1c728ad46b42e05ba7c8fe8f7170c1091b1
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-10-25 23:40:09 -0400

    teste para uso do git

--- src/main/java/com/fabio/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/fabio/libraryapi/LibraryApiApplication.java
@@ -7,12 +7,14 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApiApplication {
+
     @Bean
     public ModelMapper modelMapper(){
         return  new ModelMapper();
     }
     public static void main(String[] args) {
         SpringApplication.run(LibraryApiApplication.class, args);
+        System.out.println("OiIi");
     }
 
 }
